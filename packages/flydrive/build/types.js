dbed9ffcf984fa819e7780834e0e65c37c24526c
Ujjwal Ojha <bring2uo@gmail.com>
2020-06-14 13:57:41 +0545

    Add build

---
+++ packages/flydrive/build/types.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * @slynova/flydrive
+ *
+ * @license MIT
+ * @copyright Slynova - Romain Lanz <romain.lanz@slynova.ch>
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const LocalFileSystemStorage_1 = require("./LocalFileSystemStorage");
+//# sourceMappingURL=types.js.map
